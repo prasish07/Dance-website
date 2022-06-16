880ad8cfebb083a7e5e72d7b48c049941d2ac98c
praish <prasishshrestha0099@gmail.com>
2022-06-16 21:17:42 +0545

    Initial commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 80;
 const app = express();
 // const bodyparser = require('body-parser')  //hasnot been used in this filed
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/dance_website', {useNewUrlParser: true})
 
 
 //Define mongoose schema
@@ -19,9 +19,18 @@ var contactSchema = new mongoose.Schema({
 //Defining a model
 var Contact = mongoose.model('contact', contactSchema)
 
+var signSchema = new mongoose.Schema({
+
+    email: String,
+    password: String
+});
+
+//Defining a model
+var sign = mongoose.model('sign', signSchema)
+
 //EXPRESS SPECIFIC STUFF    
 app.use('/static', express.static('static')) //For serving static files
-// app.use(express.urlencoded())
+app.use(express.urlencoded())
 
 //PUG SPECIFIC STUFF
 app.set('view engine', 'pug') //Set the templete engine as pug
@@ -37,10 +46,27 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug',params)
 })
 
+app.get('/sign_in',(req,res)=>{
+    const params = { }
+    res.status(200).render('sign_in.pug',params)
+})
+
 app.post('/contact',(req,res)=>{
     var myData = new Contact(req.body);
     myData.save().then(()=>{
         res.send("this item has been save to the databased")
+        
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    })
+    // res.status(200).render('contact.pug',params)
+})
+
+app.post('/signin',(req,res)=>{
+    var myData = new sign(req.body);
+    myData.save().then(()=>{
+        // res.send("this item has been save to the databased")
+        res.render('home.pug')
     }).catch(()=>{
         res.status(400).send("Item was not saved to the database")
     })
